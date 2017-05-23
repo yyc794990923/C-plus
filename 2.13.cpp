8228672c325e6fe36803c55b6c4a143b21f77b3a
YuChen Yan <yanyuchen@xiyoulinux.org>
2017-05-23 21:25:09 +0800

    2.13

---
+++ 2.13.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 2.13.cpp
+	> Author: yanyuchen
+	> Mail: 794990923@qq.com
+	> Created Time: 2017年03月22日 星期三 19时07分12秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int i = 42;
+int main()
+{
+    int i = 100;
+    int j = i;
+    cout << j << endl;
+}
