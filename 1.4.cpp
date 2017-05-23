deed0005d4da426b9d9e300b769000ac79516b16
YuChen Yan <yanyuchen@xiyoulinux.org>
2017-05-23 15:46:33 +0800

    1.4

---
+++ 1.4.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: 1.4.cpp
+	> Author: yyc
+	> Mail: 794990923@qq.com
+	> Created Time: 2016年11月14日 星期一 21时28分02秒
+ ************************************************************************/
+
+#include<iostream>
+
+int main()
+{
+    std::cout << "Enter two numbers" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The Product of" << v1 << "and" << v2 << "is" << v1*v2 << std::endl;
+    return 0;
+}
+
