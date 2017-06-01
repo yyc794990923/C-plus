515c0d58c9a97c3d7d5b654babe6c4b5500cedda
YuChen Yan <yanyuchen@xiyoulinux.org>
2017-06-01 20:43:59 +0800

    3.2

---
+++ 3.2.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: 3.2.cpp
+	> Author: yanyuchen
+	> Mail: 794990923@qq.com
+	> Created Time: 2017年04月19日 星期三 21时16分56秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    cout << s << endl;
+    return 0;
+}
