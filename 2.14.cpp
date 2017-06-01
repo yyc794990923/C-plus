2a8dd2475c5484c133329c5cd42d6d778bfc77bf
YuChen Yan <yanyuchen@xiyoulinux.org>
2017-06-01 20:36:55 +0800

    2.14

---
+++ 2.14.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 2.14.cpp
+	> Author: yanyuchen
+	> Mail: 794990923@qq.com
+	> Created Time: 2017年03月22日 星期三 19时10分55秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int i = 100, sum = 0;
+int main()
+{
+    for(int i = 0; i != 10; ++i)
+    {
+        sum += i;
+    }
+    cout << i << "" << sum << endl;
+}
