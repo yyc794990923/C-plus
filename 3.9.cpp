43f09e915cddccb9eddd7e7da6715adc2a6e49cb
YuChen Yan <yanyuchen@xiyoulinux.org>
2017-06-04 21:39:38 +0800

    3.9

---
+++ 3.9.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 3.9.cpp
+	> Author: yanyuchen
+	> Mail: 794990923@qq.com
+	> Created Time: 2017年04月24日 星期一 19时36分26秒
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << s[0] << endl;
+}
